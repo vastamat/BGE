a5db60e3344054c237676521bf3e09fd7e5a89a9
VasilStamatov <v.stamatov96@gmail.com>
2019-05-12 17:36:06 +0100

    Set the logger to set the log level to error when it's not in debug mode to reduce clutter

--- bge/src/logging/Log.cpp
+++ bge/src/logging/Log.cpp
@@ -4,17 +4,24 @@
 
 namespace bge
 {
+
 std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
 std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
 void Log::Init()
 {
   spdlog::set_pattern("%^[%T] %n: %v%$");
-  s_CoreLogger = spdlog::stdout_color_mt("BGE");
-  s_CoreLogger->set_level(spdlog::level::trace);
 
+  s_CoreLogger = spdlog::stdout_color_mt("BGE");
   s_ClientLogger = spdlog::stdout_color_mt("APP");
+
+#ifdef BGE_DEBUG
+  s_CoreLogger->set_level(spdlog::level::trace);
   s_ClientLogger->set_level(spdlog::level::trace);
+#else
+  s_CoreLogger->set_level(spdlog::level::err);
+  s_ClientLogger->set_level(spdlog::level::err);
+#endif
 }
 
 } // namespace bge
